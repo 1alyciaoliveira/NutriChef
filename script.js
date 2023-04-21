485babe2d47922ec346ddbff6a9ac9a12b392169
Aly Oliveira <1alycia.oliveira@gmail.com>
2023-04-20 20:29:20 -0600

    updating HTML

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const searchForm = document.querySelector('form');
+const searchBtn = document.querySelector('search-btn');
 const searchResultDiv = document.querySelector('search-result');
 const container = document.querySelector('.container');
 let searchQuery = ''; //leave it as an empty string so it can return the value added in the search bar.
@@ -7,7 +7,7 @@ const APP_KEY = 'c99eb3d8eb3f0b6f476a1ba745ea630b';
 const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&mealtype=${mealType}`;
 
 //Press the search button after add the ingredient and make it return the value that was added.
-searchForm.addEventListener('submit', (e) => {
+searchBtn.addEventListener('click', (e) => {
     e.preventDefault(); //prevent the page to update itself
     searchQuery = e.target.querySelector('input').value;
 })
