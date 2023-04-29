9c24d22c2920c31b968442b6632203434a342d26
Andrea <andreacmz1996@gmail.com>
2023-04-29 14:35:02 -0500

    nutritional table template update

--- script.js
+++ script.js
@@ -12,47 +12,45 @@ let mealList = document.getElementById("recipe");
 searchBtn.on("click", (e) => {
   e.preventDefault();
   // searchQuery = input.value;
-  ingredients = searchQuery.val().split(',').map(word => word.trim()).join(',+');
+  ingredients = searchQuery
+    .val()
+    .split(",")
+    .map((word) => word.trim())
+    .join(",+");
   fetchRecipesAPI();
- 
 });
 
 //event listener to "I want this one" btn
-mealList.addEventListener('click', getMealRecipe);
+mealList.addEventListener("click", getMealRecipe);
 
 //save ingredients in local storage
 function ingredientStorage() {
   localStorage.setItem("ingredients", ingredients);
-};
+}
 
 //fetch the first endpoint to generate the list of recipes
-async function fetchRecipesAPI () {
+async function fetchRecipesAPI() {
   // formst url to do request
   const fetchURL = `${baseURL}&ingredients=${ingredients}`;
   // fetch recipes
   const request = await fetch(fetchURL);
 
-
   // convert request into a readable json
   const recipesJson = await request.json(); // []
-  console.log(recipesJson)
+  console.log(recipesJson);
   generateHTML(recipesJson);
 }
 
-
-//insert the HTML 
+//insert the HTML
 function generateHTML(recipesJson) {
-
   let html = "";
-
-
 
   $.each(recipesJson, function (index, recipe) {
     const title = recipe.title;
     const image = recipe.image;
     const id = recipe.id;
-    let container = $('#recipe-selection')
-    
+    let container = $("#recipe-selection");
+
     html = `
       <div class="column" data-id="${id}">
         <div class="box"> 
@@ -68,84 +66,91 @@ function generateHTML(recipesJson) {
     `;
     container.append(html);
   });
-  
+
   return html;
 }
-
 
 //fetchs the other endpoint info once we click at "I want this one" btn
 function getMealRecipe(e) {
   e.preventDefault();
-  if(e.target.classList.contains('view-recipe')) {
+  if (e.target.classList.contains("view-recipe")) {
     let mealItem = e.target.parentElement.parentElement;
     console.log(mealItem);
-    fetch(`https://api.spoonacular.com/recipes/${mealItem.dataset.id}/information/?apiKey=${APP_KEY}`)
-    .then(response => response.json())
-    .then(recipe => {generateModalHTML(recipe);
-      console.log(recipe);
-    }); 
+    fetch(
+      `https://api.spoonacular.com/recipes/${mealItem.dataset.id}/information/?apiKey=${APP_KEY}`
+    )
+      .then((response) => response.json())
+      .then((recipe) => {
+        generateModalHTML(recipe);
+        console.log(recipe);
+      });
   }
 }
 
 //Generate modal HTML
-function generateModalHTML (recipe) {
-  let generatedModalHTML = '';
+async function generateModalHTML(recipe) {
+  let generatedModalHTML = "";
   //let recipeImg = data.image;
-  let recipeName = data.title;
-  let summary = data.summary;
-  var modal = document.querySelector('.modal');
+  let recipeName = recipe.title;
+  let summary = recipe.summary;
+  var modal = document.querySelector(".modal");
+  const modalTitle = document.getElementById('modal-title')
+  const modalSummary = document.getElementById('modal-summary')
+  const nutriCalories = document.getElementById('nutri-calories')
+  // empty array ready to recive API data
+  const nutritionQuery = [];
 
-    // empty array ready to recive API data
-    const nutritionQuery = [];
+  await recipe.extendedIngredients.forEach(async (ingredient) => {
+    const ingText = ingredient.original;
 
-  
-    recipe.extendedIngredients.forEach((ingredient) => {
-      const ingText = ingredient.original;
-  
-      // send ingText to API and recive the data
-      $.ajax({
+    const response = await fetch(
+      "https://api.api-ninjas.com/v1/nutrition?query=" + ingText,
+      {
         method: "GET",
-        url: "https://api.api-ninjas.com/v1/nutrition?query=" + ingText,
         headers: { "X-Api-Key": "cDxmeJpmVNhqPAllzxJX+A==kiF4qqk9jASFyhRS" },
-        contentType: "application/json",
-        success: function (result) {
-          console.log(result[0]);
-          nutritionQuery.push(result[0])
-          console.log("nutritionQuery", nutritionQuery)
-        },
-        error: function ajaxError(jqXHR) {
-          console.error("Error: ", jqXHR.responseText);
-        },
-      });
-  
-      
-    });
+        "Content-Type": "application/json",
+      }
+    );
 
-  generatedModalHTML +=
-  `
-            <div class="modal-background"></div>
-            <div class="modal-content has-background-white">
-              <h3 class="title mb-6">${recipeName}</h3>
-              <p class="summary">${summary}</p>
-            <div class="sourceUrl">
-              <a href="${sourceUrl}" id="recipe-link">Check Recipe</a>
-            </div>
-            <button>Nutritional Facts</button>
-            </div>
-  `;
+    const result = await response.json();
+    console.log(result);
+    nutritionQuery.push(result[0]);
+  });
 
-  modal.innerHTML = generatedModalHTML;
-  
+  const sourceUrl = "";
+
+  const calories = 0;
+  const totalFats = 0;
+  const saturatedFats = 0;
+  const cholesterol = 0;
+  const sodium = 0;
+  const carbs = 0;
+  const fiber = 0;
+  const sugars = 0;
+  const protein = 0;
+
+  nutritionQuery.forEach((nutritional) => {
+    calories += nutritional["calories"];
+    carbs += nutritional["carbohydrates_total_g"];
+
+    console.log(calories)
+  });
+
+
+
+  modalTitle.innerText = recipeName
+  modalSummary.innerHTML = summary;
+
   console.log(sourceUrl);
-  
-  const modalBg = document.querySelector('.modal-background');
+
+  const modalBg = document.querySelector(".modal-background");
 
   //Open modal
-  modal.classList.add('is-active');
+  modal.classList.add("is-active");
 
   //Close modal
-  modalBg.addEventListener('click', () => {
-    modal.classList.remove('is-active');
+  modalBg.addEventListener("click", () => {
+    modal.classList.remove("is-active");
   });
 
   const link = document.getElementById("recipe-link");
